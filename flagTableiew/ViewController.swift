538fa040a2ce7a887215bb13b1654a143e419477
jantiks <58886410+jantiks@users.noreply.github.com>
2020-11-25 12:49:25 +0400

    Initial Commit

---
+++ flagTableiew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flagTableiew
+//
+//  Created by Tigran on 11/25/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
